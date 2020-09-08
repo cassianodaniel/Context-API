7213279c5a303368803aea6a17ace3a5d4765a08
Daniel Chaves <daniel.chaves@digivox.com.br>
2020-09-08 09:42:18 -0300

    Refactoring Typescript

--- tscontextapi/src/App.tsx
+++ tscontextapi/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-import CountProvider from "./context/Context";
+import CountProvider from "./context/CountContext";
 
 import Counter from "./components/Counter";
 import DecreaseCounter from "./components/DecreaseCounter";
